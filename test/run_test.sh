526b552cd0e2aed833c19e61833fa6d1738a7a5b
Songtao Gui <guisongtao0104@gmail.com>
2023-12-04 17:16:37 +0800

    Update run_test.sh

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,3 +1,4 @@
-seq 100 | while read a; do echo -e "$a\n$a" > $a.txt; done &&\
+num=2000 # set the number of files to paste
+seq $num | while read a; do echo -e "$a\n$a" > $a.txt; done &&\
 mpaste -d '\t' -t 1 --header 's/^/SMP/; s/\.txt//;' -v *.txt -o test.out
 # rm -f *.txt
